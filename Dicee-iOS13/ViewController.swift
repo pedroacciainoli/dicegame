8d2eb69bd10ff98b9d4a0dd9484f716b33207acf
pedro.acciainoli <pedro.acciainoli@gmail.com>
2022-05-02 00:45:07 -0300

    add total function

--- Dicee-iOS13/ViewController.swift
+++ Dicee-iOS13/ViewController.swift
@@ -11,12 +11,14 @@ import UIKit
 class ViewController: UIViewController {
     @IBOutlet weak var diceImageView1: UIImageView!
     @IBOutlet weak var diceImageView2: UIImageView!
+    @IBOutlet weak var labelTotal: UILabel!
     
+    @IBOutlet weak var labelTotal2: UILabel!
     
-
     override func viewDidLoad() {
         super.viewDidLoad()
         //need to use #imageLiteral( to showup images
+        labelTotal.text = "0"
         
         // Do any additional setup after loading the view.
     }
@@ -24,10 +26,14 @@ class ViewController: UIViewController {
     @IBAction func rollButtonPressd(_ sender: UIButton) {
         
       let diceArray =  [#imageLiteral(resourceName: "dice1") , #imageLiteral(resourceName: "dice2") , #imageLiteral(resourceName: "dice3") , #imageLiteral(resourceName: "dice4") , #imageLiteral(resourceName: "dice5") , #imageLiteral(resourceName: "dice6")]
-    
-        diceImageView1.image = diceArray.randomElement()
-        diceImageView2.image = diceArray.randomElement()
-    }
-    
-}
+ 
+        let left = Int.random(in: 0...5)
+        let right = Int.random(in: 0...5)
+        diceImageView1.image = diceArray[left]
+        diceImageView2.image = diceArray[right]
+        
+        labelTotal.text = "\((left+1)+(right+1))"
 
+    }
+
+}
