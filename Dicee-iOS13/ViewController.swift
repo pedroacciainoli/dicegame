d4536a310a11c630c5df2fc1cc94d56da43ec00c
pedro.acciainoli <pedro.acciainoli@gmail.com>
2022-05-01 22:42:47 -0300

    add random dice

--- Dicee-iOS13/ViewController.swift
+++ Dicee-iOS13/ViewController.swift
@@ -11,18 +11,23 @@ import UIKit
 class ViewController: UIViewController {
     @IBOutlet weak var diceImageView1: UIImageView!
     @IBOutlet weak var diceImageView2: UIImageView!
+    
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
         //need to use #imageLiteral( to showup images
-        diceImageView1.image = #imageLiteral(resourceName: "dice6")
-        diceImageView2.image = #imageLiteral(resourceName: "dice2")
+        
         // Do any additional setup after loading the view.
     }
 
     @IBAction func rollButtonPressd(_ sender: UIButton) {
-        diceImageView1.image = #imageLiteral(resourceName: "dice4")
-        diceImageView2.image = #imageLiteral(resourceName: "dice4")
+        
+      let diceArray =  [#imageLiteral(resourceName: "dice1") , #imageLiteral(resourceName: "dice2") , #imageLiteral(resourceName: "dice3") , #imageLiteral(resourceName: "dice4") , #imageLiteral(resourceName: "dice5") , #imageLiteral(resourceName: "dice6")]
+    
+        diceImageView1.image = diceArray[Int.random(in: 0...5)]
+        diceImageView2.image = diceArray[Int.random(in: 0...5)]
+        
     }
     
 }
